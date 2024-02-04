d0f502d069da84551c8fe041425a7381204634b4
danondanissimo <106009630+danondanissimo@users.noreply.github.com>
2024-02-04 14:14:37 +0000

    Deploying to gh-pages from @ danondanissimo/goit-js-hw-11@4aaa5cac3112b5a0835ccf4bcf212dc1060c6037 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,4 +1,4 @@
-import{S as i}from"./assets/vendor-b41a7778.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))s(t);new MutationObserver(t=>{for(const r of t)if(r.type==="childList")for(const a of r.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&s(a)}).observe(document,{childList:!0,subtree:!0});function n(t){const r={};return t.integrity&&(r.integrity=t.integrity),t.referrerpolicy&&(r.referrerPolicy=t.referrerpolicy),t.crossorigin==="use-credentials"?r.credentials="include":t.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function s(t){if(t.ep)return;t.ep=!0;const r=n(t);fetch(t.href,r)}})();const l=document.querySelector(".js-search-form"),c=document.querySelector(".js-photo-container");l.addEventListener("submit",o=>{o.preventDefault();const e=o.target.elements.query.value;u(e).then(n=>{m(n)})});function u(o){const e="https://pixabay.com/api/",n=`?key=42185111-4f5cd61d4ffab1c12875fcbb6&q=${o}&image_type=photo&orientation=horizontal&safesearch=true`,s=e+n;return fetch(s).then(t=>{if(t.status===404)throw new Error(response.status);return t.json()})}function f(o){return o.hits.map(e=>`<li class="gallery-item"><a class="gallery-link" href="${e.largeImageURL}">
+import{S as l}from"./assets/vendor-b41a7778.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))i(t);new MutationObserver(t=>{for(const s of t)if(s.type==="childList")for(const n of s.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&i(n)}).observe(document,{childList:!0,subtree:!0});function r(t){const s={};return t.integrity&&(s.integrity=t.integrity),t.referrerpolicy&&(s.referrerPolicy=t.referrerpolicy),t.crossorigin==="use-credentials"?s.credentials="include":t.crossorigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function i(t){if(t.ep)return;t.ep=!0;const s=r(t);fetch(t.href,s)}})();const o=document.querySelector(".js-search-form"),c=document.querySelector(".js-photo-container");o.addEventListener("submit",a=>{a.preventDefault();const e=a.target.elements.query.value;m(e).then(r=>{f(r)})});function m(a){const e="https://pixabay.com/api/",r=`?key=42185111-4f5cd61d4ffab1c12875fcbb6&q=${a}&image_type=photo&orientation=horizontal&safesearch=true`,i=e+r;return fetch(i).then(t=>{if(t.status===404)throw new Error(response.status);return t.json()})}function u(a){return a.hits.map(e=>`<li class="gallery-item"><a class="gallery-link" href="${e.largeImageURL}">
     <img
       src="${e.webformatURL}"
       data-source="${e.largeImageURL}"
@@ -7,9 +7,19 @@ import{S as i}from"./assets/vendor-b41a7778.js";(function(){const e=document.cre
       
     /></a>  
 
-  <div class="image-stats">
-<p>
-      Likes:${e.likes}, Views:${e.views}, Comments:${e.comments}, Downloads:${e.downloads}
-    </p>
-  </div></li>`).join("")}function m(o){const e=f(o);c.insertAdjacentHTML("beforeend",e)}new i(".gallery a",{captionsData:"alt",captionDelay:250});
+  <div class="image-stats-block">
+<ul class="image-stats-list">
+  <li class="image-stats-item"><p class="image-stats-text">Likes:<br><span class="image-stats-value">${e.likes}</span></p>
+</li>
+<li class="image-stats-item">
+  <p class="image-stats-text">Views:<br><span class="image-stats-value">${e.views}</span></p>
+</li>
+<li class="image-stats-item">
+  <p class="image-stats-text">Comments:<br><span class="image-stats-value">${e.comments}</span></p>
+</li>
+<li class="image-stats-item">
+  <p class="image-stats-text">Downloads:<br><span class="image-stats-value">${e.downloads}</span></p>
+</li>
+</ul>
+  </div></li>`).join("")}function f(a){const e=u(a);c.insertAdjacentHTML("beforeend",e)}new l(".gallery a",{captionsData:"alt",captionDelay:250});
 //# sourceMappingURL=commonHelpers.js.map
