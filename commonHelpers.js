64cbf7b6d22a8ed90e7a7f31144631ab4a10b894
danondanissimo <106009630+danondanissimo@users.noreply.github.com>
2024-02-04 14:57:11 +0000

    Deploying to gh-pages from @ danondanissimo/goit-js-hw-11@1f28ce41c3c4a8b87d04f727b7612ccb349f6f94 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,25 +1,25 @@
-import{S as l}from"./assets/vendor-b41a7778.js";(function(){const e=document.createElement("link").relList;if(e&&e.supports&&e.supports("modulepreload"))return;for(const t of document.querySelectorAll('link[rel="modulepreload"]'))i(t);new MutationObserver(t=>{for(const s of t)if(s.type==="childList")for(const n of s.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&i(n)}).observe(document,{childList:!0,subtree:!0});function r(t){const s={};return t.integrity&&(s.integrity=t.integrity),t.referrerpolicy&&(s.referrerPolicy=t.referrerpolicy),t.crossorigin==="use-credentials"?s.credentials="include":t.crossorigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function i(t){if(t.ep)return;t.ep=!0;const s=r(t);fetch(t.href,s)}})();const o=document.querySelector(".js-search-form"),c=document.querySelector(".js-photo-container");o.addEventListener("submit",a=>{a.preventDefault();const e=a.target.elements.query.value;m(e).then(r=>{f(r)})});function m(a){const e="https://pixabay.com/api/",r=`?key=42185111-4f5cd61d4ffab1c12875fcbb6&q=${a}&image_type=photo&orientation=horizontal&safesearch=true`,i=e+r;return fetch(i).then(t=>{if(t.status===404)throw new Error(response.status);return t.json()})}function u(a){return a.hits.map(e=>`<li class="gallery-item"><a class="gallery-link" href="${e.largeImageURL}">
+import{S as l}from"./assets/vendor-b41a7778.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const n of s.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&i(n)}).observe(document,{childList:!0,subtree:!0});function r(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerpolicy&&(s.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?s.credentials="include":e.crossorigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function i(e){if(e.ep)return;e.ep=!0;const s=r(e);fetch(e.href,s)}})();const o=document.querySelector(".js-search-form"),c=document.querySelector(".js-photo-container");o.addEventListener("submit",a=>{a.preventDefault();const t=a.target.elements.query.value;m(t).then(r=>{d(r)})});function m(a){const t="https://pixabay.com/api/",r=`?key=42185111-4f5cd61d4ffab1c12875fcbb6&q=${a}&image_type=photo&orientation=horizontal&safesearch=true`,i=t+r;return fetch(i).then(e=>{if(e.ok)return e.json();throw new Error(e.status)})}function u(a){return a.hits.map(t=>`<li class="gallery-item"><a class="gallery-link" href="${t.largeImageURL}">
     <img
-      src="${e.webformatURL}"
-      data-source="${e.largeImageURL}"
-      alt="${e.tags}"
+      src="${t.webformatURL}"
+      data-source="${t.largeImageURL}"
+      alt="${t.tags}"
       class="gallery-image"
       
     /></a>  
 
   <div class="image-stats-block">
 <ul class="image-stats-list">
-  <li class="image-stats-item"><p class="image-stats-text">Likes:<br><span class="image-stats-value">${e.likes}</span></p>
+  <li class="image-stats-item"><p class="image-stats-text">Likes:<br><span class="image-stats-value">${t.likes}</span></p>
 </li>
 <li class="image-stats-item">
-  <p class="image-stats-text">Views:<br><span class="image-stats-value">${e.views}</span></p>
+  <p class="image-stats-text">Views:<br><span class="image-stats-value">${t.views}</span></p>
 </li>
 <li class="image-stats-item">
-  <p class="image-stats-text">Comments:<br><span class="image-stats-value">${e.comments}</span></p>
+  <p class="image-stats-text">Comments:<br><span class="image-stats-value">${t.comments}</span></p>
 </li>
 <li class="image-stats-item">
-  <p class="image-stats-text">Downloads:<br><span class="image-stats-value">${e.downloads}</span></p>
+  <p class="image-stats-text">Downloads:<br><span class="image-stats-value">${t.downloads}</span></p>
 </li>
 </ul>
-  </div></li>`).join("")}function f(a){const e=u(a);c.insertAdjacentHTML("beforeend",e)}new l(".gallery a",{captionsData:"alt",captionDelay:250});
+  </div></li>`).join("")}const f=new l(".gallery a",{captionsData:"alt",captionDelay:250});function d(a){const t=u(a);c.insertAdjacentHTML("beforeend",t)}f.refresh();
 //# sourceMappingURL=commonHelpers.js.map
